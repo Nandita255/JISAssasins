b9598f5807b45f3230e01094fe5fbb5d7f32f342
Nandita255 <46235851+Nandita255@users.noreply.github.com>
2018-12-29 14:41:32 +0530

    Add files via upload

---
+++ Assignment18.py
@@ -0,0 +1,7 @@
+#Q18. Write a program to calculate the x to the power y without using standard functions
+num=int(input("enter a number"))
+num2=int(input("enter 2nd number"))
+a=1
+for num2 in range (1,num2+1):
+    a=(a*num)
+print(a)
